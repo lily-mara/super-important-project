a0d31800e2764bd33323355106add37aaf68d377
balchdl <balchdl@h-84f1.lab8.csi.miamioh.edu>
2015-02-05 21:19:06 -0500

    Fixed the bugs in the Fibonnaci sequence program.

--- java/Fib.java
+++ java/Fib.java
@@ -1,26 +1,21 @@
 public class Fib {
-	public static void ma1n(String[] args) {
+	public static void main(String[] args) {
 		System.out.println(fib(100));
 	}
 
 	public static int fib(int fibNumber) {
-		switch (fibNumber) {
-			case 0:
-				return 0;
-			case 1:
-				return 1;
+		if (fibNumber == 0 || fibNumber == 1) {
+			return fibNumber;
+		} else {
+			int n = 0;
+			int n1 = 1;
+			int n2 = 0;
+			for (int i = 2; i < (fibNumber + 1); i++) {
+				n2 = n1;
+				n = n1 + n2;
+				n1 = n;
+			}
+			return n;
 		}
-
-		int n = 0;
-		int n1 = 1;
-		int n2 = 0;
-
-		for (int i = 2; i < fibNumber + 1; i++) {
-			n2 = n1;
-			n = n1 + n2;
-			n1 = n;
-		}
-
-		return n;
 	}
 }
